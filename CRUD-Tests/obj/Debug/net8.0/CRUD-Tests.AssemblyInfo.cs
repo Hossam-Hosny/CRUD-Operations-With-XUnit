2a2d8dcbcff6f06c989b3d00a25daa875d50fa43
Hossam Hosny <HossamHosny415@gamil.com>
2025-01-22 23:53:15 +0200

    Complete implementing and testing crud operations

--- CRUD-Tests/obj/Debug/net8.0/CRUD-Tests.AssemblyInfo.cs
+++ CRUD-Tests/obj/Debug/net8.0/CRUD-Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRUD-Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e1cd36c329e67b67ce7599097e48ebac38cbc12")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRUD-Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRUD-Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ServiceContracts/obj/Debug/net8.0/ServiceContracts.AssemblyInfo.cs
+++ ServiceContracts/obj/Debug/net8.0/ServiceContracts.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServiceContracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e1cd36c329e67b67ce7599097e48ebac38cbc12")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServiceContracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServiceContracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
